f12e5f8c9a54b7a3e2b55953f8f2f58ac1036212
Ogungbemi Gbenga <adaradenu01@gmail.com>
2023-05-19 19:20:37 -0700

    fixing codes

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -5,8 +5,7 @@ import sys
 
 
 def fizzbuzz(n):
-    """
-    FizzBuzz function prints numbers from 1 to n separated by a space.
+    """ FizzBuzz function prints numbers from 1 to n separated by a space.
 
     - For multiples of three print "Fizz" instead of the number and for
       multiples of five print "Buzz".
